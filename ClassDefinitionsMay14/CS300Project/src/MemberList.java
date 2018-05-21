c2786f848675e0061750627b2150f5a58584ce86
Danc2050 <32185616+Danc2050@users.noreply.github.com>
2018-05-20 18:47:11 -0700

    Update MemberList.java

--- ClassDefinitionsMay14/CS300Project/src/MemberList.java
+++ ClassDefinitionsMay14/CS300Project/src/MemberList.java
@@ -4,7 +4,7 @@ public class MemberList {
     /*public MemberList(){this.m_root = null;)
     public int read_from_file(){}
     public int write_to_file(){}//TODO Try catch blocks in imp.
-    public int add_member(){}
-    public int update_member(){}
-    public int remove_member(){}*/
+    public int add{}
+    public int update{}
+    public int remove{}*/
 }
