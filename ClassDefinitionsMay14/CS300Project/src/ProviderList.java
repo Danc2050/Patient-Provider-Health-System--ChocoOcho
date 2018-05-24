8e56cbc698bdd4e73f19d8a80dce5dc2d17b415b
Danc2050 <32185616+Danc2050@users.noreply.github.com>
2018-05-23 21:05:49 -0700

    Update ProviderList.java

--- ClassDefinitionsMay14/CS300Project/src/ProviderList.java
+++ ClassDefinitionsMay14/CS300Project/src/ProviderList.java
@@ -1,15 +1,13 @@
 import java.io.*;
 
-public class ProviderList extends Utility{
+public class ProviderList extends Utility {
     protected Node p_root;
 
-    public ProviderList()
-    {
+    public ProviderList() {
         this.p_root = null;
     }
 
-    public int read_from_file()
-    {
+    public int read_from_file() {
         String file_name = "/Users/dabartagus/IdeaProjects/ChocoOcho/providerList.txt";
         try {
 
@@ -25,12 +23,11 @@ public class ProviderList extends Utility{
                 float temp_week_fee = Float.parseFloat(columns[2]);
                 int temp_num_consultations = Integer.parseInt(columns[3]);
 
-                this.p_root = add_provider(this.p_root, temp_name, temp_id,  temp_week_fee, temp_num_consultations);
+                this.p_root = add_provider(this.p_root, temp_name, temp_id, temp_week_fee, temp_num_consultations);
                 line = in.readLine();
             }
 
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found.");
         } catch (IOException ex) {
             System.out.println("Error reading file.");
@@ -40,24 +37,59 @@ public class ProviderList extends Utility{
 
     public int write_to_file()
     {
-        return -1;
+        try {
+            FileWriter writer = new FileWriter("ProviderList.txt");
+            this.write_to_file(p_root, writer);
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public int write_to_file(Node s_root, FileWriter file)
+    {
+        if (s_root == null)
+            return 1;
+
+        write_to_file(s_root.go_left(), file);
+        try {
+            file.write(s_root.get_service_code() + ";");
+            file.write(s_root.get_service_name() + ";");
+            file.write(s_root.get_service_fee() + ";");
+            file.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        write_to_file(s_root.go_right(), file);
+        return 1;
     }
 
     public int add_provider() // wrapper
     {
-        return -1;
+        System.out.println("\nEnter provider name");
+        String name = input.nextLine();
+        System.out.println("\nEnter provider I.D.");
+        int id = input.nextInt();
+        System.out.println("\nEnter number of consultations.");
+        int consultations = input.nextInt();
+
+        add_provider(p_root, name, id, 0, consultations);
+        return 1;
     }
 
-    protected Node add_provider(Node p_root, String t_name, int t_id, float t_week_fee, int t_num_consulations){
+    protected Node add_provider(Node p_root, String t_name, int t_id, float t_week_fee, int t_num_consultations){
         if(p_root == null) {
-            p_root = new Provider(t_name, t_id, t_week_fee, t_num_consulations);
+            p_root = new Provider(t_name, t_id, t_week_fee, t_num_consultations);
             return p_root;
         }
         else {
-            if (p_root.get_provider_name().compareToIgnoreCase(t_name) < 0)
-                p_root.connect_left(add_provider(p_root.go_left(), t_name, t_id, t_week_fee, t_num_consulations));
+            if (p_root.get_provider_name().compareToIgnoreCase(t_name) < 1)
+                p_root.connect_left(add_provider(p_root.go_left(), t_name, t_id, t_week_fee, t_num_consultations));
             else
-                p_root.connect_right(add_provider(p_root.go_left(), t_name, t_id, t_week_fee, t_num_consulations));
+                p_root.connect_right(add_provider(p_root.go_left(), t_name, t_id, t_week_fee, t_num_consultations));
         }
         return p_root;
     }
