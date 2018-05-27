e577fb0ff6b0598c4af07d920d1fff99c330861c
tttnvndbtdln <phuong@pdx.edu>
2018-05-27 14:37:39 -0700

    Updated Menu

--- ChocoOchoMay21/CS300Project/src/Menu.java
+++ ChocoOchoMay21/CS300Project/src/Menu.java
@@ -9,11 +9,12 @@ public class Menu extends Utility {
         int response = 0;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("\nChoose a mode.");
+        System.out.println("\n\nChoose a mode.");
         System.out.println("\n[1] - Interactive/Manager Mode" + "\n[2] - Provider Mode.");
 
         while (response < 1 || response > 2)
         {
+            System.out.println("\nEnter response: ");
             response = input.nextInt();
             input.nextLine();
         }
@@ -24,72 +25,177 @@ public class Menu extends Utility {
     public static int intMode()
     {
         //Implement a use... Example: call an add function with string "member" or "provider". Or create Member or Provider object and pass in to use Dynamic binding.
-        Scanner input = new Scanner(System.in);
         int response = 0;
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("What do you want to access?");
-        System.out.println("[1] - Member." + "\n[2] - Provider." + "\n[3] - Manager.");
+        System.out.println("\n\nWhat do you want to access?");
+        System.out.println("[1] - Member." + "\n[2] - Provider." + "\n[3] - Report");
+
+        while (response < 1 || response > 3) {
+            System.out.println("\nEnter response: ");
+            response = input.nextInt();
+            input.nextLine();
+        }
+        return response;
+    }
+
+    public int intMode_Member(MemberList list_of_members)
+    {
+        int member_response = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n\nWhat would you like to do?");
+        System.out.println("[1] - Add Member." + "\n[2] - Remove Member." + "\n[3] - Update Member");
+
+        while (member_response < 1 || member_response > 3)
+        {
+            System.out.println("\nEnter response: ");
+            member_response = input.nextInt();
+            input.nextLine();
+        }
+
+        /*if (member_response == 1) //Add Member
+        {
+            if (list_of_members.add_member())
+            {
+                //Success/Fail
+            }
+        }
+
+        else if (member_response == 2) //Remove Member
+        {
+            if (list_of_members.remove_member())
+            {
+                //Success/Fail
+            }
+        }
+
+        else
+        {
+            if (list_of_members.update_member())
+            {
+                //Success/Fail
+            }
+        }*/
+        return 0;
+    }
+
+    public int intMode_Provider(ProviderList list_of_providers)
+    {
+        int provider_response = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What would you like to do?");
+        System.out.println("[1] - Add Provider." + "\n[2] - Remove Provider." + "\n[3] - Update Provider");
+
+        while (provider_response < 1 || provider_response > 3)
+        {
+            System.out.println("\nEnter response: ");
+            provider_response = input.nextInt();
+            input.nextLine();
+        }
+
+        /*if (provider_response == 1) //Add Provider
+        {
+            if (list_of_providers.add_provider())
+                //Success/Fail
+        }
+
+        else if (provider_response == 2)
+        {
+            if (list_of_providers.remove_provider())
+        }
+
+        else
+        {
+            if (list_of_providers.update_provider())
+        }*/
+        return 0;
+    }
+
+    public int intMode_Report()
+    {
+        int report_response = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What would you like to do?");
+        System.out.println("[1] Print Provider Report." + "\n[2] Print Patient Report.");
+
+        while (report_response < 1 || report_response > 2)
+        {
+            System.out.println("\nEnter response:");
+            report_response = input.nextInt();
+            input.nextLine();
+        }
+
+        if (report_response == 1)
+        {
+            //TODO Print Provider Report
+        }
+        else if (report_response == 2)
+        {
+            //TODO Print Patient Report
+        }
+    }
+
+
+    /*The Provider Menu.*/
+    public int providerMenu()
+    {
+        int response = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n\nChoose from the options below." +
+                "\n[1] - Provider Directory." + "\n[2] - Create New Service." +
+                "\n[3] - Access ChocAn Billing Services.");
 
         while (response < 1 || response > 3)
         {
+            System.out.println("\nEnter response: ");
             response = input.nextInt();
             input.nextLine();
         }
 
         if (response == 1)
         {
-            //Member options.
-            System.out.println("What would you like to do?");
-            System.out.println("[1] - Add." + "\n[2] - Remove." + "\n[3] - Update");
+            //TODO Access Provider Directory on file
         }
 
-        if (response == 2)
+        else if (response == 2)
         {
-            //Provider options.
+            //TODO Create new service
         }
 
-        else {
-            //Manager options.
-
-            System.out.println("Choose from the options below.");
-            System.out.println("[1] - Add." + "\n[2] - Remove." + "\n[3] - Update");
-            int job = input.nextInt();
-            input.nextLine();
+        else
+        {
+            //TODO Access ChocAn Billing Services
         }
 
         return 0;
     }
 
-    /*The Provider Menu.*/
-    public static int providerMenu()
-    {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Choose from the options below." + "\n[1] - Provider Directory." + "\n[2] - Create Member Report." + "\n[3] - Access ChocAn Billing Services.");
-        return input.nextInt();
-    }
     /*Verifies a user is in the system (print statements and if conditionals only).*/
     public static boolean verifyW()
     {
+        int id = 0;
         Scanner input = new Scanner(System.in);
+
         System.out.println("Slide member card into terminal (enter I.D. #).");
-        int id = input.nextInt();
+        id = input.nextInt();
         input.nextLine();
-                /*if(verify() == 1)//Member is active.
-                 *{
-                 * 		System.out.println("Validated");
-                 * 		return true;
-                 * }
-                 *else if(verify() == -1)
-                 *{
-                 *		System.out.println("Member Suspended");
-                 *		return 0;
-                 *}
-                 *else
-                 *{
-                 *		System.out.println("Invalid number")
-                 *		return 0;
-                 *}
-                 */
-        return true;
+
+        if (verify(id) == 1)
+        {
+            System.out.println("Validated.");
+            return true;
+        }
+
+        else if (verify(id) == -1)
+            System.out.println("Member suspended.");
+
+        else
+            System.out.println("Invalid ID number.");
+
+        return false;
     }
 }
