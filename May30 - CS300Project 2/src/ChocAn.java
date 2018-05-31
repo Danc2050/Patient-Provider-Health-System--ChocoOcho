ff94496cba52999e8372624fdcde66330f1de775
Daniel Connelly <32185616+Danc2050@users.noreply.github.com>
2018-05-30 17:51:53 -0700

    Update ChocAn.java

--- May30 - CS300Project 2/src/ChocAn.java
+++ May30 - CS300Project 2/src/ChocAn.java
@@ -95,48 +95,5 @@ public class ChocAn {
             else
                 main_menu = 1;
         }
-
-        //Marko's and Angelic's
-
-        /*ServiceList test = new ServiceList();
-        test.read_from_file();
-        test.display_all();
-        test.add_service();
-        test.add_service();
-        test.display_all();*/
     }
 }
-
-/*
-    public static void main(String[] args) {
-        int x = mainMode();
-//Switch #1 -- main mode.
-        switch(x)
-        {
-            case 0: x = mainMode(); System.out.println("\nX: " + x); break;
-            case 1: System.out.println("You are in interactive mode.");
-                intMode();
-                break;
-            case 2: System.out.println("You are in Provider/Manager mode.");
-                if(verifyW())
-                {
-                    int y = providerMenu();
-                    switch(y)
-                    {
-                        case 1: //providerDir(); break;
-                        case 2: //memReport(); break;
-                        case 3: //bill(); break;
-                        default: System.out.println("\nIncorrect entry");
-                    }
-                    break;
-                }
-                else//return to terminal
-                    x = 0; break;
-            case 3: System.out.println("You are in manager mode.");
-            default: System.out.println("\nIncorrect entry.");
-                break;
-        };
-    }
-*/
-
-
