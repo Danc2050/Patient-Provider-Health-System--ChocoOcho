cf82929f74c32fc161e39715d414fc18f6ad320a
angelicphan <38331202+angelicphan@users.noreply.github.com>
2018-06-02 22:10:08 -0700

    Update ServiceHistory.java

--- May30 - CS300Project 2/src/ServiceHistory.java
+++ May30 - CS300Project 2/src/ServiceHistory.java
@@ -1,42 +1,11 @@
 public class ServiceHistory extends Node{
-    //protected Node s_root;
-    /*protected String comments;
-    protected String s_name;
-    protected int s_code;
-    protected float s_fee;
-    protected String service_date;
-    protected String log_date;
-    protected String p_name;
-    protected String m_name;
-    protected int p_num;
-    protected int m_num;
-    protected Address p_ad;
-    protected Address m_ad;*/
+
     protected Provider provider;
     protected Member member;
     protected Service service;
     protected String service_date;
     protected String log_date;
     protected String comments;
-
-    /*public ServiceHistory(String comments, String s_name, int s_code, float s_fee, String service_date,String log_date, String p_name, String m_name, int p_num, int m_num,Address pad,Address mad) {
-        this.comments = comments;
-        this.s_name = s_name;
-        this.s_code = s_code;
-        this.s_fee = s_fee;
-        this.service_date = service_date;
-        this.log_date = log_date;
-        this.p_name = p_name;
-        this.m_name = m_name;
-        this.p_num = p_num;
-        this.m_num = m_num;
-        /*if(this.p_ad == null)
-           this.p_ad = new Address();
-        this.p_ad.to_copy(pad);
-        if(this.m_ad == null)
-            this.m_ad = new Address();
-        this.m_ad.to_copy(mad);*/
-    //}
 
     public ServiceHistory(Provider p, Member m, Service s, String sdate, String ldate, String tcomments){
         if(this.provider == null)
@@ -97,8 +66,7 @@ public class ServiceHistory extends Node{
     public void set_service_code(int new_code){return;}
     public void set_service_fee(float new_fee){return;}
     public void set_m_name(String newName){return;}
-    public float get_week_fee(){return 0;}
-    public int get_num_consultations(){return 0;}
+
     public void set_p_name(String newName){return;}
     public void set_provider_id(int newId) {return;}
     public int get_provider_id() {return 0;}
