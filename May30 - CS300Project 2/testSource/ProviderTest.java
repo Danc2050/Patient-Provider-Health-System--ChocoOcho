70b7b13e728f1fe9837b7943a066948bdb48c3b3
angelicphan <38331202+angelicphan@users.noreply.github.com>
2018-06-06 23:56:02 -0700

    Update ProviderTest.java

--- May30 - CS300Project 2/testSource/ProviderTest.java
+++ May30 - CS300Project 2/testSource/ProviderTest.java
@@ -1,10 +1,25 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProviderTest {
 
     @Test
     void toCopy() {
+        Address a = new Address("123 Main St.", "Springfield", "OR", 97209);
+        Provider p = new Provider("Joe", 12345, a, "stuff");
+
+        Provider p2 = new Provider();
+        p2.toCopy(p);
+
+        assertEquals("Joe", p2.Name);
+        assertEquals(12345, p2.id);
+        Address ad = p.get_address();
+        assertEquals(a.street, ad.street);
+        assertEquals(a.state, ad.state);
+        assertEquals(a.city, ad.city);
+        assertEquals(a.zip, ad.zip);
+        assertEquals("stuff", p2.Service);
     }
 
     @Test
